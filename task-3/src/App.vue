cd104183997d2cdea6b9608c54e7c2c397c6f445
Natallia Pazniak <natalyua_nata_@mail.ru>
2026-02-22 20:44:43 +0300

    init: task-3

---
+++ task-3/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import AlertBox from './components/AlertBox.vue'
+</script>
+
+<template>
+  <div class="page">
+    <AlertBox>
+      Something bad happened.
+    </AlertBox>
+  </div>
+</template>
+
+<style scoped>
+.page {
+  min-height: 100dvh;
+  display: grid;
+  place-items: center;
+  padding: 24px;
+}
+</style>
---
+++ task-3/src/components/AlertBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="alert-box">
+    <strong>Error!</strong>
+    <br />
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.alert-box {
+  color: rgb(102, 102, 102);
+  border: 2px solid rgb(255, 0, 0);
+  border-radius: 8px;
+  padding: 32px;
+  background-color: rgb(248, 248, 248);
+  font-size: 20px;
+  max-width: 520px;
+}
+  
+strong {
+  color: rgb(255, 0, 0);
+  font-size: 24px;
+}
+</style>
