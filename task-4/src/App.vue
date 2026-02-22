e1bfdc3e3df042ac0aea2ef3f246555e3a1e4b56
Natallia Pazniak <natalyua_nata_@mail.ru>
2026-02-22 21:38:24 +0300

    init: task-4

---
+++ task-4/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import ButtonCounter from './components/ButtonCounter.vue'
+</script>
+
+<template>
+  <div class="page">
+    <h1>Here are many child components!</h1>
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+    <ButtonCounter />
+     <ButtonCounter />
+  </div>
+</template>
+
+<style scoped>
+.page {
+  min-height: 100dvh;
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ task-4/src/components/ButtonCounter.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <button @click="count++">
+    You clicked me {{ count }} times.
+  </button>
+</template>
+
+<style scoped>
+button {
+  background-color: rgb(255, 105, 180);
+  color: rgb(255, 255, 255);
+  border: none;
+  border-radius: 6px;
+  padding: 10px 16px;
+  font-size: 16px;
+}
+</style>
