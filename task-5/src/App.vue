147815316bfc084934aedd6d386d3f3cb27e8a37
Natallia Pazniak <natalyua_nata_@mail.ru>
2026-02-24 20:45:11 +0300

    init: task-5

---
+++ task-5/src/App.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue';
+import CustomInput from './components/CustomInput.vue';
+
+const message = ref('hello');
+</script>
+
+<template>
+  <div class="app">
+    <div class="form">
+      <p class="label">Введите ваш текст:</p>
+      <CustomInput v-model="message" />
+      <p class="value">{{ message }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.app {
+  min-height: 100dvh;
+  display: grid;
+  place-items: center;
+
+}
+
+.form {
+  display: grid;
+  gap: 12px;
+  width: min(420px, 100%);
+}
+
+.label {
+  margin: 0;
+  font-size: 16px;
+}
+
+.value {
+  margin: 0;
+  font-size: 14px;
+  opacity: 0.7;
+}
+</style>
---
+++ task-5/src/components/CustomInput.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({
+  modelValue: String, 
+});
+
+const emit = defineEmits(['update:modelValue']); 
+
+const updateValue = (event) => {
+  emit('update:modelValue', event.target.value); 
+};
+</script>
+
+<template>
+  <input
+    :value="props.modelValue"  
+    @input="updateValue"        
+    class="custom-input"
+  />
+</template>
+
+<style scoped>
+.custom-input {
+  width: 100%;
+  padding: 10px 12px;
+  border: 1px solid rgba(214, 214, 214, 1);
+  border-radius: 8px;
+  font-size: 16px;
+  outline: none;
+}
+
+.custom-input:focus {
+  border-color: rgba(255, 79, 163, 1);
+  box-shadow: 0 0 0 3px rgba(255, 79, 163, 0.2);
+}
+</style>
