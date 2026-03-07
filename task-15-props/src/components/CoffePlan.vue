a4c980a9c0cc9bc752515df9d1ab6812075ed925
Natallia Pazniak <natalyua_nata_@mail.ru>
2026-03-07 22:11:07 +0300

    init: props-task

---
+++ task-15-props/src/components/CoffePlan.vue
@@ -0,0 +1,13 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    default: "Default Plan",
+  },
+});
+</script>
+<template>
+  <div class="description">
+    <span class="title"> {{ name }} </span>
+  </div>
+</template>
---
+++ task-15-props/src/components/CoffeePlan.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="plan">
+    <div class="description">
+      <span class="title">{{ name }}</span>
+    </div>
+  </div>
+</template>
+<script setup>
+defineProps({
+  name: String,
+})
+</script>
