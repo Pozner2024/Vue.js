d2b3e54977af9cf1e404c962bd0a8c45c3214826
Natallia Pazniak <natalyua_nata_@mail.ru>
2026-03-08 20:44:39 +0300

    init: task-16

---
+++ task-16-githab/src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import GithabCard from "./components/GithabCard.vue";
+</script>
+
+<template>
+  <div class="app">
+    <h1>My Project</h1>
+    <GithabCard username="Pozner2024" />
+  </div>
+</template>
---
+++ task-16-githab/src/components/GithabCard.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { ref } from "vue"
+
+const props = defineProps({
+  username: {
+    type: String,
+    required: true
+  }
+})
+
+const user = ref()
+
+fetch(`https://api.github.com/users/${props.username}`).then(async (res) => {
+  const data = await res.json()
+  user.value = data
+})
+</script>
+
+<template>
+  <div v-if="user" class="card card-side">
+    <figure>
+      <img :src="user.avatar_url" alt="Movie" />
+    </figure>
+    <div class="card-body">
+      <h2 class="card-title">{{ user.name }}</h2>
+      <p>
+        <strong>Followers:</strong> {{ user.followers }}
+        <strong>Following:</strong> {{ user.following }}
+      </p>
+      <div class="card-actions">
+        <a :href="user.html_url" class="btn">View Profile</a>
+      </div>
+    </div>
+  </div>
+</template>
